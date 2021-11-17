d269ea4a50933abd23ccf8056b05bc4a509befe8
sanjaykumar3519 <clashofsanjay@gmail.com>
2021-11-17 08:19:21 +0530

    removing unused statement

--- app/src/main/java/com/example/cha/LoginFragment.java
+++ app/src/main/java/com/example/cha/LoginFragment.java
@@ -1,6 +1,5 @@
 package com.example.cha;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -10,7 +9,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -42,72 +40,59 @@ public class LoginFragment extends Fragment {
         //progress bar
         progressBar = v.findViewById(R.id.progress);
 
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //progress bar
-                progressBar.setVisibility(View.VISIBLE);
-                sField = new String[2];
-                sData = new String[2];
-                sField[0] = "username";
-                sField[1] = "password";
-                sData[0] = String.valueOf(username.getText());
-                sData[1] = String.valueOf(password.getText());
-                if(!sData[0].isEmpty() && !sData[1].isEmpty())
-                {
-                    //login task
-                    Handler handler = new Handler(Looper.getMainLooper());
-                    handler.post(new Runnable() {
-                        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-                        @Override
-                        public void run() {
-                            Database database = new Database("http://192.168.1.37/hunt/Login.php", sField, sData);
-                            if(database.onStart())
+        login.setOnClickListener(v1 -> {
+            //progress bar
+            progressBar.setVisibility(View.VISIBLE);
+            sField = new String[2];
+            sData = new String[2];
+            sField[0] = "username";
+            sField[1] = "password";
+            sData[0] = String.valueOf(username.getText());
+            sData[1] = String.valueOf(password.getText());
+            if(!sData[0].isEmpty() && !sData[1].isEmpty())
+            {
+                //login task
+                Handler handler = new Handler(Looper.getMainLooper());
+                handler.post(() -> {
+                    Database database = new Database("http://192.168.1.37/hunt/Login.php", sField, sData);
+                    if(database.onStart())
+                    {
+                        if(database.onComp())
+                        {
+                            String temp = database.getData();
+                            if(temp.equals("Login Success"))
                             {
-                                if(database.onComp())
+                                if(loginCallBack!=null)
                                 {
-                                    String temp = database.getData();
-                                    if(temp.equals("Login Success"))
-                                    {
-                                        if(loginCallBack!=null)
-                                        {
-                                            Toast.makeText(getContext(),temp,Toast.LENGTH_SHORT).show();
-                                            loginCallBack.setData(sData[0]);
-                                            loginCallBack.callBacks("main");
-                                        }
-                                    }
-                                    else
-                                    {
-                                        Toast.makeText(v.getContext(),temp,Toast.LENGTH_SHORT).show();
-                                        progressBar.setVisibility(View.GONE);
-                                    }
+                                    Toast.makeText(getContext(),temp,Toast.LENGTH_SHORT).show();
+                                    loginCallBack.setData(sData[0]);
+                                    loginCallBack.callBacks("main");
                                 }
                             }
+                            else
+                            {
+                                Toast.makeText(v1.getContext(),temp,Toast.LENGTH_SHORT).show();
+                                progressBar.setVisibility(View.GONE);
+                            }
                         }
-                    });
-                }
-                else {
-                    Toast.makeText(getContext(), "Fields are empty", Toast.LENGTH_SHORT).show();
-                    progressBar.setVisibility(View.GONE);
-                }
+                    }
+                });
+            }
+            else {
+                Toast.makeText(getContext(), "Fields are empty", Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.GONE);
             }
         });
 
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(loginCallBack!=null){
-                    loginCallBack.callBacks("signup");
-                }
+        signup.setOnClickListener(v12 -> {
+            if(loginCallBack!=null){
+                loginCallBack.callBacks("signup");
             }
         });
 
-        forgot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(loginCallBack!=null){
-                    loginCallBack.callBacks("forgot");
-                }
+        forgot.setOnClickListener(v13 -> {
+            if(loginCallBack!=null){
+                loginCallBack.callBacks("forgot");
             }
         });
         return v;
